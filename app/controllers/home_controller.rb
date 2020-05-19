eb153323cca70128b18c2fd88892883014aa85f0
Sunny Asar <ring2rings93@gmail.com>
2020-05-19 17:08:05 +0100

    add data scrapping

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+  require 'httparty'
+  require 'nokogiri'
+  require 'byebug'
+
+
+  def index
+    
+    # byebug
+    # puts parsed_page
+  end
+
+  def show
+  end
+end
